3548669efaebc630394516b8b4c7abefe691acfd
HyunSeokJang99 <hs.jang@teamsparta.co>
2021-12-12 20:59:01 +0900

    mongodb 접속 설정 변경

--- src/models/mongoose.js
+++ src/models/mongoose.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
 
 const mongoose = require("mongoose");
-// const connectionURL = "mongodb://127.0.0.1:27017" || process.env.MONGOURL;
-const connectionURL = process.env.MONGOURL;
+const connectionURL =
+    "mongodb://127.0.0.1:27017/rental_cloth" || process.env.MONGOURL;
+// const connectionURL =
+// "mongodb://admin:admin@54.180.150.229:27017/rental_cloth?authSource=admin&authMechanism=SCRAM-SHA-1";
 
-mongoose.connect(`${connectionURL}/rental_cloth`, {
+mongoose.connect(`${connectionURL}`, {
     useNewUrlParser: true,
 });
 
-const db = mongoose.connection;
+let db = mongoose.connection;
 db.on("error", function () {
     console.log("Connection Failed!");
 });
