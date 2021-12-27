da08657b7a53a4267a277ab2203fd7e7bb606675
DanielJang99 <daniel.jang.0621@gmail.com>
2021-12-27 14:38:48 +0900

    fix: 로깅 시 환경변수 참조 잠시 삭제

--- src/index.js
+++ src/index.js
@@ -17,10 +17,7 @@ morgan.format("myformat", ":date | :method :url | :status | :response-time ms");
 
 app.use(
     morgan("myformat", {
-        // skip: function (req, res) {
-        //     return res.statusCode < 400;
-        // }, // http return 이 에러일때만 출력
-        stream: logger.stream, // logger에서 morgan의 stream 을 받도록 추가
+        stream: logger.stream,
     }),
 );
 
--- src/logs/logger.js
+++ src/logs/logger.js
@@ -2,22 +2,20 @@ const appRoot = require("app-root-path");
 const winston = require("winston");
 require("winston-daily-rotate-file");
 
-if (process.env.NODE_ENV == "production") {
-    const transport = new winston.transports.DailyRotateFile({
-        filename: `${appRoot}/src/logs/%DATE%.log`,
-        maxsize: 1024,
-        datePatten: "YYYY-MM-DD-HH",
-        timestamp: function () {
-            return Date.now();
-        },
-    });
-    let logger = winston.createLogger({
-        transports: [transport],
-    });
-    logger.stream = {
-        write: function (message, encoding) {
-            logger.info(message);
-        },
-    };
-    module.exports = logger;
-}
+const transport = new winston.transports.DailyRotateFile({
+    filename: `${appRoot}/src/logs/%DATE%.log`,
+    maxsize: 1024,
+    datePatten: "YYYY-MM-DD-HH",
+    timestamp: function () {
+        return Date.now();
+    },
+});
+let logger = winston.createLogger({
+    transports: [transport],
+});
+logger.stream = {
+    write: function (message, encoding) {
+        logger.info(message);
+    },
+};
+module.exports = logger;
